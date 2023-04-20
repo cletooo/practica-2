bbf8d2e9c6421c8ad741090b0b7c02c6a1e596ff
cletooo <128733571+cletooo@users.noreply.github.com>
2023-04-20 11:56:41 -0600

    Add files via upload

---
+++ practica 2/practica 2.1.js
@@ -0,0 +1,53 @@
+const a= true && true;
+const b= true && false;
+const c= false && true;
+const d= false && (3==4);
+const e= "cat" && "dog";
+const f= false && "cat";
+const g= "cat" && false;
+
+
+
+
+
+
+const tiempo = 14;
+let saludo;
+if (tiempo<12) {
+saludo = "buenos dias";   
+}else if (tiempo>12 && tiempo<19){
+saludo = "buenas tardes"
+}else if (tiempo >19 && tiempo <24) {
+    saludo = "buenas noches"
+    
+} 
+console.log(saludo);
+
+//switch =  5;
+let texto; 
+switch (dia) {
+    case 0:
+    texto = "domingo";
+    break;
+    case 1:
+    texto = "lunes";
+    break;
+    case 2:
+    texto = "martes";
+    break;
+    case 3:
+    texto = "miercoles";
+    break;
+    case 4:
+    texto = "jueves";
+    break;
+    case 5:
+    texto = "viernes";
+    break;
+    case 6:
+    texto = "sabado";
+    break;
+    
+}
+
+    
